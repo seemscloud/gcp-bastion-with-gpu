4da46404effcedfac94f22d76c69680c1a9de8d4
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-04-17 12:08:08 +0200

    asd

--- bastion/instance.tf
+++ bastion/instance.tf
@@ -33,9 +33,17 @@ resource "google_compute_instance" "aaa_instance_aaa" {
     private_key = "${file("~/.ssh/id_rsa")}"
   }
 
+  # count = 0
+
   provisioner "remote-exec" {
     inline = [
-      "touch /tmp/file",
+      "sudo -u root apt-get update",
+      "sudo -u root apt-get install -y ca-certificates curl gnupg lsb-release git",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -u root gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo -u root tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo -u root apt-get update",
+      "sudo -u root apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "sudo -u root find ~/ -mindepth 1 -maxdepth 1 -exec rm -rf {} \\;"
     ]
   }
 }
